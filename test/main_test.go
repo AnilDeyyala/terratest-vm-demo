cb5b32c648ce140bdfa7f0563d618f4e11fa893d
AnilDeyyala <anil.deyyala@tigeranalytics.com>
2023-01-23 20:08:28 +0530

    Update main_test.go

--- test/main_test.go
+++ test/main_test.go
@@ -12,8 +12,8 @@ import (
 
 func TestTerraformAzureVmExample(t *testing.T) {
 	t.Parallel()
-	// ARM_SUBSCRIPTION_ID := "71ae4048-2e46-4255-8eca-c47663aa8f0c"
-	ARM_SUBSCRIPTION_ID := "71ae4048-2e46-4255-8eca-c47663aa8f0c"
+
+	subscriptionID := "71ae4048-2e46-4255-8eca-c47663aa8f0c"
 	uniquePostfix := random.UniqueId()
 
 	// Configure Terraform setting up a path to Terraform code.
@@ -22,7 +22,7 @@ func TestTerraformAzureVmExample(t *testing.T) {
 		TerraformDir: "../",
 		// Variables to pass to our Terraform code using -var options.
 		Vars: map[string]interface{}{
-			"postfix": uniquePostfix,
+			"postfix":         uniquePostfix,
 			"subscription_id": subscriptionID,
 		},
 	}
@@ -34,13 +34,13 @@ func TestTerraformAzureVmExample(t *testing.T) {
 	terraform.InitAndApply(t, terraformOptions)
 
 	// Run tests for the Virtual Machine.
-	testInformationOfVM(t, terraformOptions, ARM_SUBSCRIPTION_ID)
-	testDisksOfVM(t, terraformOptions, ARM_SUBSCRIPTION_ID)
-	testNetworkOfVM(t, terraformOptions, ARM_SUBSCRIPTION_ID)
+	testInformationOfVM(t, terraformOptions, subscriptionID)
+	testDisksOfVM(t, terraformOptions, subscriptionID)
+	testNetworkOfVM(t, terraformOptions, subscriptionID)
 }
 
 // These tests check information directly related to the specified Azure Virtual Machine.
-func testInformationOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCRIPTION_ID string) {
+func testInformationOfVM(t *testing.T, terraformOptions *terraform.Options, subscriptionID string) {
 	// Run `terraform output` to get the values of output variables.
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	virtualMachineName := terraform.Output(t, terraformOptions, "vm_name")
@@ -51,21 +51,21 @@ func testInformationOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_
 	expectedVMTags := terraform.OutputMap(t, terraformOptions, "vm_tags")
 
 	// Check if the Virtual Machine exists.
-	assert.True(t, azure.VirtualMachineExists(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID))
+	assert.True(t, azure.VirtualMachineExists(t, virtualMachineName, resourceGroupName, subscriptionID))
 
 	// Check the Admin User of the VM.
-	actualVM := azure.GetVirtualMachine(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualVM := azure.GetVirtualMachine(t, virtualMachineName, resourceGroupName, subscriptionID)
 	actualVmAdminUser := *actualVM.OsProfile.AdminUsername
 	assert.Equal(t, expectedVmAdminUser[0], actualVmAdminUser)
 
 	// Check the Storage Image properties of the VM.
-	actualImage := azure.GetVirtualMachineImage(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualImage := azure.GetVirtualMachineImage(t, virtualMachineName, resourceGroupName, subscriptionID)
 	assert.Contains(t, expectedImageSKU[0], actualImage.SKU)
 	assert.Contains(t, expectedImageVersion[0], actualImage.Version)
 
 	// Check the Availability Set of the VM.
 	// The AVS ID returned from the VM is always CAPS so ignoring case in the assertion.
-	actualexpectedAvsName := azure.GetVirtualMachineAvailabilitySetID(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualexpectedAvsName := azure.GetVirtualMachineAvailabilitySetID(t, virtualMachineName, resourceGroupName, subscriptionID)
 	assert.True(t, strings.EqualFold(expectedAvsName, actualexpectedAvsName))
 
 	// Check the assigned Tags of the VM, assert empty if no tags.
@@ -77,7 +77,7 @@ func testInformationOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_
 // The following Terratest Azure module is utilized in addition to the compute module:
 // - disk
 // See the terraform_azure_disk_example_test.go for other related tests.
-func testDisksOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCRIPTION_ID string) {
+func testDisksOfVM(t *testing.T, terraformOptions *terraform.Options, subscriptionID string) {
 	// Run `terraform output` to get the values of output variables.
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	virtualMachineName := terraform.Output(t, terraformOptions, "vm_name")
@@ -86,11 +86,11 @@ func testDisksOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCR
 	expectedDiskType := terraform.Output(t, terraformOptions, "managed_disk_type")
 
 	// Check the OS Disk name of the VM.
-	actualOSDiskName := azure.GetVirtualMachineOSDiskName(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualOSDiskName := azure.GetVirtualMachineOSDiskName(t, virtualMachineName, resourceGroupName, subscriptionID)
 	assert.Equal(t, expectedOSDiskName, actualOSDiskName)
 
 	// Check the VM Managed Disk exists in the list of all VM Managed Disks.
-	actualManagedDiskNames := azure.GetVirtualMachineManagedDisks(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualManagedDiskNames := azure.GetVirtualMachineManagedDisks(t, virtualMachineName, resourceGroupName, subscriptionID)
 	assert.Contains(t, actualManagedDiskNames, expectedDiskName)
 
 	// Check the Managed Disk count of the VM.
@@ -99,7 +99,7 @@ func testDisksOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCR
 
 	// Check the Disk Type of the Managed Disk of the VM.
 	// This does not apply to VHD disks saved under a storage account.
-	actualDisk := azure.GetDisk(t, expectedDiskName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualDisk := azure.GetDisk(t, expectedDiskName, resourceGroupName, subscriptionID)
 	actualDiskType := actualDisk.Sku.Name
 	assert.Equal(t, expectedDiskType, actualDiskType)
 }
@@ -110,7 +110,7 @@ func testDisksOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCR
 // - publicaddresss
 // - virtualnetwork
 // See the terraform_azure_network_example_test.go for other related tests.
-func testNetworkOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBSCRIPTION_ID string) {
+func testNetworkOfVM(t *testing.T, terraformOptions *terraform.Options, subscriptionID string) {
 	// Run `terraform output` to get the values of output variables.
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
 	virtualMachineName := terraform.Output(t, terraformOptions, "vm_name")
@@ -122,16 +122,16 @@ func testNetworkOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBS
 
 	// VirtualNetwork and Subnet tests
 	// Check the Subnet exists in the Virtual Network.
-	actualVnetSubnets := azure.GetVirtualNetworkSubnets(t, expectedVNetName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualVnetSubnets := azure.GetVirtualNetworkSubnets(t, expectedVNetName, resourceGroupName, subscriptionID)
 	assert.NotNil(t, actualVnetSubnets[expectedVNetName])
 
 	// Check the Private IP is in the Subnet Range.
-	actualVMNicIPInSubnet := azure.CheckSubnetContainsIP(t, expectedPrivateIPAddress, expectedSubnetName, expectedVNetName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualVMNicIPInSubnet := azure.CheckSubnetContainsIP(t, expectedPrivateIPAddress, expectedSubnetName, expectedVNetName, resourceGroupName, subscriptionID)
 	assert.True(t, actualVMNicIPInSubnet)
 
 	// Network Interface Card tests
 	// Check the VM Network Interface exists in the list of all VM Network Interfaces.
-	actualNics := azure.GetVirtualMachineNics(t, virtualMachineName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualNics := azure.GetVirtualMachineNics(t, virtualMachineName, resourceGroupName, subscriptionID)
 	assert.Contains(t, actualNics, expectedNicName)
 
 	// Check the Network Interface count of the VM.
@@ -139,12 +139,12 @@ func testNetworkOfVM(t *testing.T, terraformOptions *terraform.Options, ARM_SUBS
 	assert.Equal(t, expectedNICCount, len(actualNics))
 
 	// Check for the Private IP in the NICs IP list.
-	actualPrivateIPAddress := azure.GetNetworkInterfacePrivateIPs(t, expectedNicName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualPrivateIPAddress := azure.GetNetworkInterfacePrivateIPs(t, expectedNicName, resourceGroupName, subscriptionID)
 	assert.Contains(t, actualPrivateIPAddress, expectedPrivateIPAddress)
 
 	// Public IP Address test
 	// Check for the Public IP for the NIC. No expected value since it is assigned runtime.
-	actualPublicIP := azure.GetIPOfPublicIPAddressByName(t, expectedPublicAddressName, resourceGroupName, ARM_SUBSCRIPTION_ID)
+	actualPublicIP := azure.GetIPOfPublicIPAddressByName(t, expectedPublicAddressName, resourceGroupName, subscriptionID)
 	assert.NotNil(t, actualPublicIP)
 
 }
